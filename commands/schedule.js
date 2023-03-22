fd3225455896dc0f1524dea4f684d5bb7e43a4c9
zevinal <ariankrasniqi23@gmail.com>
2023-03-22 00:35:27 +0000

    Added a reaction collector for a reminder func

--- commands/schedule.js
+++ commands/schedule.js
@@ -28,9 +28,26 @@ module.exports = {
 			.setDescription(`${title} has been added to the calendar!`)
 			.addFields(
 				{ name: 'Time', value: `${eventTime}` },
+				{ name: 'Reminder?', value: 'React with a 👍 below if you want to be sent a reminder in DM 1 hour before the event.' },
 			)
 			.setTimestamp();
 		// Reply to user with embedded message
-		await interaction.reply({ embeds: [embed] });
+		const message = await interaction.reply({ embeds: [embed], fetchReply: true });
+		message.react('👍');
+
+		const filter = (reaction, user) => {
+			return reaction.emoji.name == '👍' && user.id == interaction.user.id;
+		};
+
+		const collector = message.createReactionCollector({ filter, time: 60000 });
+
+		collector.on('collect', (reaction, user) => {
+			console.log(`Collected ${reaction.emoji.name} from ${user.tag}`);
+		});
+
+		collector.on('end', collected => {
+			console.log(`Collected ${collected.size} items`);
+		});
+
 	},
 };
