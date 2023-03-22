673d2ff1944f863984957b0dd2c444987e9a30c8
zevinal <ariankrasniqi23@gmail.com>
2023-03-22 11:57:58 +0000

    Added build file, and build and start scripts.

---
+++ build.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+const config = {
+	'token': 'your-token-here',
+	'clientId': 'your-client-id',
+	'guildId': 'your-guild-id',
+};
+
+fs.access('config.json', fs.constants.F_OK, (err) => {
+	if (err) {
+		fs.writeFile('config.json', JSON.stringify(config, null, 2), (err) => {
+			if (err) throw err;
+			console.log('The file "config.json" has been created!');
+		});
+	}
+	else {
+		console.log('The file "config.json" already exists, no action required.');
+	}
+});
