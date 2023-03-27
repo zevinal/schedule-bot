14d864a50d91c24719148eb5f0eb0813555d0e45
zevinal <ariankrasniqi23@gmail.com>
2023-03-27 09:42:20 +0100

    Added bar graph to availability command.

--- commands/calendar.js
+++ commands/calendar.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } = require('discord.js');
 const chrono = require('chrono-node');
+const buttonCounts = {};
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -8,6 +9,9 @@ module.exports = {
 	async execute(interaction) {
 		console.log('Executing weekdates command...');
 		console.log('Interaction received:', interaction);
+
+		// NOTE: THIS COMMAND WAS BUILT WITH ONLY FIVE PEOPLE IN MIND. IT CAN ONLY REGISTER UP TO TEN VOTES PER DATE LISTED
+		// Getting next week's days and dates
 		const now = new Date();
 		const today = now.getDay();
 		const daysUntilNextWeek = 7 - today;
@@ -18,6 +22,7 @@ module.exports = {
 			nextWeekDates.push(nextDay.toDateString());
 		}
 
+		// Creating the rows of buttons
 		const row1 = new ActionRowBuilder()
 			.addComponents(
 				nextWeekDates.slice(0, 5).map(date => {
@@ -29,6 +34,7 @@ module.exports = {
 						.setStyle(ButtonStyle.Primary);
 				}),
 			);
+		// Two rows are needed as only 5 buttons fit on each
 		const row2 = new ActionRowBuilder()
 			.addComponents(
 				nextWeekDates.slice(5, 7).map(date => {
@@ -40,23 +46,39 @@ module.exports = {
 						.setStyle(ButtonStyle.Primary);
 				}),
 			);
+
+
+		// Set up initial graph
+		let graph = '';
+		const blackBlock = '　';
+		const whiteBlock = '▇';
+		for (const date of nextWeekDates) {
+			const count = 0;
+			const blocks = blackBlock.repeat(10 - count) + whiteBlock.repeat(count);
+			graph += `${date}: ${blocks} ${count}\n`;
+		}
+
+		// Create message embed with the following week's dates and a bar graph
 		const embed = new EmbedBuilder()
 			.setColor('#0099ff')
 			.setTitle('Next Session')
-			.setDescription('Here are the dates for next week, select your most preferred day:');
+			.setDescription('Here are the dates for next week, select your most preferred day(s):')
+			.addFields({ name: 'Availability', value: '```' + graph + '```' });
 
-		await interaction.reply ({ embeds: [embed], components: [row1, row2] });
+		// Reply to user with embedded message
+		await interaction.reply({ embeds: [embed], components: [row1, row2] });
 		console.log('Reply sent.');
+
 
 		const filter = (reaction) => {
 			return nextWeekDates.includes(reaction.customId);
 		};
 
 		const collector = interaction.channel.createMessageComponentCollector({ filter });
-
-		const buttonCounts = {};
-		collector.on('collect', (available) => {
+		collector.on('collect', async (available) => {
 			console.log('Button clicked:', available.customId);
+
+			// Getting the click count on each of the buttons
 			const buttonId = available.customId;
 			if (!buttonCounts[buttonId]) {
 				buttonCounts[buttonId] = 1;
@@ -64,8 +86,28 @@ module.exports = {
 			else {
 				buttonCounts[buttonId]++;
 			}
+
+			// Update graph with the new click count
+			const date = new Date(buttonId).toDateString();
+			const lineIndex = nextWeekDates.findIndex(d => d === date);
+			const line = graph.split('\n')[lineIndex];
+			const countIndex = line.indexOf(blackBlock);
+			const oldCount = parseInt(line.slice(countIndex + 1));
+			const newCount = oldCount + 1;
+			const newLine = line.slice(0, countIndex) + whiteBlock + blackBlock.repeat(10 - newCount) + ` ${newCount}`;
+			const updatedLines = graph.split('\n').map((l, i) => i === lineIndex ? newLine : l);
+			graph = updatedLines.join('\n');
+
+
+			// Update the embedded message
+			const updatedEmbed = new EmbedBuilder(embed)
+				.spliceFields(0, 1)
+				.addFields({ name: 'Availability', value: '```' + graph + '```' });
+
+			await available.update({ embeds: [updatedEmbed] });
 			console.log(`${buttonId} button has been clicked ${buttonCounts[buttonId]} times.`);
 		});
+
 
 		collector.on('end', () => {
 			console.log('Button click collection ended.');
