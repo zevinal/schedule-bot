b346464049118c6f152e51b764bf2e274a9dd649
zevinal <ariankrasniqi23@gmail.com>
2023-03-28 06:17:59 +0100

    Added function to remove user id from saved event

--- commands/schedule.js
+++ commands/schedule.js
@@ -2,8 +2,45 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const chrono = require('chrono-node');
 const fs = require('fs');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
 const scheduleFile = path.join(__dirname, '../schedule.json');
+let schedule = [];
+try {
+	const data = fs.readFileSync(scheduleFile, 'utf-8');
+	schedule = JSON.parse(data);
+	console.log(typeof schedule, schedule);
+	if (typeof schedule !== 'object' || Array.isArray(schedule)) {
+		schedule = [];
+		console.log(typeof schedule, schedule);
+	}
+	console.log(typeof schedule, schedule);
+}
+catch (error) {
+	console.error(`Failed to read schedule file: ${error}`);
+}
+
+// Load schedule from file
+function loadSchedule() {
+	try {
+		const data = fs.readFileSync(scheduleFile, 'utf-8');
+		const loadedSchedule = JSON.parse(data);
+		if (Array.isArray(loadedSchedule)) {
+			schedule = loadedSchedule;
+		}
+	}
+	catch (error) {
+		console.error(`Failed to read schedule file: ${error}`);
+	}
+}
+
+// Save schedule to file
+function saveSchedule() {
+	fs.writeFileSync(scheduleFile, JSON.stringify(schedule, null, 2));
+}
+
+// Load schedule on startup
+loadSchedule();
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -43,7 +80,7 @@ module.exports = {
 			return reaction.emoji.name == '👍' && user.id != message.author.id;
 		};
 
-		const collector = message.createReactionCollector({ filter });
+		const collector = message.createReactionCollector({ filter, dispose: true });
 
 		const users = new Set();
 
@@ -79,20 +116,53 @@ module.exports = {
 
 					// Add the user to the set of users who reacted with a 👍
 					users.add(user);
+					const userIds = Array.from(users, (user) => user.id);
 
 					// Store the event name, time, and the users who reacted in schedule.json
-					const schedule = {
-						eventName: title,
-						eventTime: eventTime.toISOString(),
-						users: Array.from(users, user => user.id),
-					};
-					fs.writeFileSync(scheduleFile, JSON.stringify(schedule));
+
+					// Check if the event already exists in schedule.json
+					const existingScheduleItem = schedule.find((item) => item.eventName === title && item.eventTime === eventTime.toISOString());
+					if (existingScheduleItem) {
+						existingScheduleItem.users.push(user.id);
+					}
+					else {
+
+						// Create a new schedule item if the event doesn't exist already
+						const newScheduleItem = {
+							id: uuidv4(),
+							eventName: title,
+							eventTime: eventTime.toISOString(),
+							users: userIds,
+						};
+						schedule.push(newScheduleItem);
+					}
+
+					// Write the updated schedule to schedule.json
+					fs.writeFileSync(scheduleFile, JSON.stringify(schedule, null, 2));
+
+					// Save schedule.json after writing
+					saveSchedule();
 				}
 				catch (error) {
 					console.error(`Could not send DM to user ${user.tag}.`, error);
 				}
 			}
 		});
+
+		// Define handler for removing a reaction
+		collector.on('remove', async (reaction, user) => {
+			if (reaction.emoji.name === '👍') {
+				console.log(`Removed ${reaction.emoji.name} from ${user.tag}.`);
+
+				// Find the schedule item for the removed reaction
+				const scheduleItem = schedule.find((item) => item.eventName === title && item.eventTime === eventTime.toISOString());
+				scheduleItem.users = scheduleItem.users.filter((id) => id !== user.id);
+
+				// Save schedule.json after updating
+				saveSchedule();
+			}
+		});
+
 		const reminderTime = new Date(eventTime.getTime() - 60 * 60 * 1000);
 		const now = new Date();
 		const delay = reminderTime.getTime() - now.getTime();
